b9c8954530560e581e8283795cbbbf82c813799d
Kurvora <Kurvora_n@mail.ru>
2024-06-11 19:23:35 +0300

    Add  form

--- script.js
+++ script.js
@@ -22,9 +22,68 @@ function closeDistrictPopup() {
 
 // Скачать информацию о районе
 function downloadDistrictInfo() {
-    // Здесь добавьте логику для скачивания информации
     alert("Downloading district information...");
 }
 
 // Добавляем обработчик события для изображения
 document.querySelector('.button-image').addEventListener('click', togglePopup);
+
+
+// Открыть попап с расчетами
+function openCalculatePopup() {
+    document.getElementById("calculate-popup").style.display = "flex";
+}
+
+// Закрыть попап с расчетами
+function closeCalculatePopup() {
+    document.getElementById("calculate-popup").style.display = "none";
+}
+
+// Рассчитать данные и показать в попапе
+function calculate() {
+    // Пример расчетов (можете заменить на свои)
+    const result = "Результаты ваших расчетов";
+
+    // Показать результат в попапе
+    document.getElementById("calculation-result").innerText = result;
+
+    // Открыть попап
+    openCalculatePopup();
+}
+
+// Перетаскивание попапа
+dragElement(document.getElementById("draggable-popup"));
+
+function dragElement(element) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    element.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Получить начальные позиции курсора
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Рассчитать новую позицию курсора
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // Установить новую позицию элемента
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
